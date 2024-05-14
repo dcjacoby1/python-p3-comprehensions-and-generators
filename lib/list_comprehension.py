5372d9e49eedcfc364852aa30966a08841e7b8cf
Dan <jacobydan01@gmail.com>
2024-05-14 11:38:14 -0400

    listComprehensions

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+#function returns list all the even numbers in num_list
 def return_evens(num_list):
-    pass
+    return [num for num in num_list if num % 2 == 0]
 
+#adds exclamation to each string in list 
 def make_exclamation(sentence_list):
-    pass
+    return [word + "!" for word in sentence_list]
